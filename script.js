d56c1f7872b46b79c4935040cf2be0a035ef35a6
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-05-11 17:08:19 +0530

    add simple code

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-function getUserInfo() {
-  return new Promise(function (res, rej) {
-    setTimeout(() => {
-      res({ id: 1, name: "John Doe" });
-    }, 2000);
-  });
-}
+// function getUserInfo() {
+//   return new Promise(function (res, rej) {
+//     setTimeout(() => {
+//       res({ id: 1, name: "John Doe" });
+//     }, 2000);
+//   });
+// }
 
-function getUserPics(id) {
-  return new Promise(function (res, rej) {
-    res([{ url: "pic1" }, { url: "pic2" }]);
-  });
-}
+// function getUserPics(id) {
+//   return new Promise(function (res, rej) {
+//     res([{ url: "pic1" }, { url: "pic2" }]);
+//   });
+// }
 
-getUserInfo()
-  .then(function (data) {
-    console.log(data);
-    return getUserPics(data.id);
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+// getUserInfo()
+//   .then(function (data) {
+//     console.log(data);
+//     return getUserPics(data.id);
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
+
