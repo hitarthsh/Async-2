5c1a4f834aef7f1bb1715a4b9e3c869a5c60c935
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-05-11 17:01:09 +0530

    add code

---
+++ script.js
@@ -0,0 +1,22 @@
+function getUserInfo() {
+  return new Promise(function (res, rej) {
+    setTimeout(() => {
+      res({ id: 1, name: "John Doe" });
+    }, 2000);
+  });
+}
+
+function getUserPics(id) {
+  return new Promise(function (res, rej) {
+    res([{ url: "pic1" }, { url: "pic2" }]);
+  });
+}
+
+getUserInfo()
+  .then(function (data) {
+    console.log(data);
+    return getUserPics(data.id);
+  })
+  .then(function (data) {
+    console.log(data);
+  });
